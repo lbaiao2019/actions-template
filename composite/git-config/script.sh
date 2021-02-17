e09659e34f04e8a01d7a20e362400cbd6adf1db4
Leonardo Baiao <lbaiao@morneaushepell.com>
2021-02-17 21:07:22 +0000

    GitConfig

--- composite/git-config/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-VAR1=$1
-mkdir -p /home/runner/.ssh
-echo "$VAR1" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-echo -e "Host github.com\n\tHostName github.com\n\tPreferredAuthentications publickey\n\tIdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-chmod 600 ~/.ssh/config
-ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
